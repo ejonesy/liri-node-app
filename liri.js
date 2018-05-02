a37dd7747be7df2d334e2267d7930ad298fd9335
Eve Jones <evejones@reimageds-MacBook-Pro.local>
2018-05-02 09:50:02 -0700

    Some minor but effective adjustments

--- liri.js
+++ liri.js
@@ -1,7 +1,35 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var fs = require("fs");
+var Twitter = require('twitter')
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 
 var client = new Twitter(keys.twitter);
+
+var getTweets = function() {
+    var params = {screen_name: 'eveonice'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response)     {
+        console.log(JSON.stringify(tweets, null, 0));
+
+        if (!error) {
+        for (i=0; i<20; i++){
+            //console.log(JSON.parse(tweets).text);
+                }
+            }
+        })
+};
+
+getTweets();
+
+
+// fs.readFile("keys.js", "utf8", function(error, data){
+//     if (error) {
+//         return console.log(error);
+//     }
+
+//     console.log(data);
+//     var dataArr = data.split(",");
+//     console.log(dataArr);
+// })
